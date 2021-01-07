8eba6905e32ec7abaf36e05d63407ae784f91100
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-01-06 23:27:54 -0600

    Working DTOs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+// import { collection, query, startAt, order, limit } from 'typesaurus';
+
 import { Button } from '@material-ui/core';
 
 import firebase from 'firebase/app';
-import 'firebase/firebase-storage';
 import 'firebase/auth';
+import 'firebase/firestore'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useDownloadURL } from 'react-firebase-hooks/storage';
 
 firebase.initializeApp({
     apiKey: "AIzaSyAqJ3rBF_ynBL0_v-d8khudDauw6Ug-1PA",
@@ -20,14 +22,14 @@ firebase.initializeApp({
 });
 
 const auth = firebase.auth();
-const store = firebase.storage();
+const db = firebase.firestore();
 
 function App() {
     const [user] = useAuthState(auth);
     return (
         <div className="App">
             <section>
-                {user ? <MainView user={user} /> : <SignIn />}
+                {user ? <MainView /> : <SignIn />}
             </section>
         </div>
     );
@@ -43,15 +45,62 @@ function SignIn() {
     );
 }
 
-function MainView(props: { user: any }) {
+interface CA {
+    createdAt: firebase.firestore.Timestamp,
+}
+
+interface Screenshot extends CA {
+    cnnFileName: string,
+    foxFileName: string,
+}
+
+interface DTO extends CA {
+    cnnFilePath: string,
+    foxFilePath: string,
+}
+
+function MainView() {
+    const [dtos, setDtos] = useState<DTO[]>([]);
+
+    async function getData() {
+        let query = db.collection('screenshots').orderBy('createdAt', 'desc').limit(10);
+        const lastElt = dtos[dtos.length - 1];
+        query = lastElt ? query.startAfter(lastElt.createdAt) : query;
+        const data = await query.get();
+
+        const tempDtos: DTO[] = [];
+        data.forEach(doc => {
+            const temp = doc.data() as Screenshot;
+            tempDtos.push({
+                createdAt: temp.createdAt,
+                cnnFilePath: getImageUrl(temp.cnnFileName),
+                foxFilePath: getImageUrl(temp.foxFileName),
+            });
+        });
+        
+        setDtos(dtos.concat(tempDtos));
+    }
+
     return (
         <>
-            <p>
-                {JSON.stringify(props.user)}
-            </p>
+            {dtos?.map(
+                ({ createdAt, cnnFilePath, foxFilePath }, i) => (
+                    <React.Fragment key={i}>
+                        <p>Created At: {createdAt.toDate().toLocaleString()}</p>
+                        <img src={cnnFilePath} alt="cnn" />
+                        <img src={foxFilePath} alt="fox" />
+                        <hr />
+                    </React.Fragment>
+                )
+            )}
+            <Button color="primary" onClick={() => getData()}>Get 10 More</Button>
             <SignOut />
         </>
     );
+}
+
+function getImageUrl(fileName: string) {
+    return `https://firebasestorage.googleapis.com/v0/b/headline-archiver.appspot.com/o/screenshots%2F${fileName}?alt=media`;
 }
 
 function SignOut() {
